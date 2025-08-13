37573dc9c421017e3029b3d0f7d8e76c761ed79f
AnsumanP <p.ansuman1991@gmail.com>
2025-08-13 22:28:45 +0530

    End of section 2

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<AppUser> Users{ get; set; }
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+}
